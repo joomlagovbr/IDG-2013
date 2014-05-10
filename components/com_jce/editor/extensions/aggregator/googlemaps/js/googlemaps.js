16357cd707902a96dc0caf9b41642049be068683
Rafael Berlanda <rafaelberlanda@gmail.com>
2014-05-10 10:46:42 -0300

    Retirada do editor JCE. Os usuários que pretenderem utiliza-lo deverao configurar suas instancias locais

--- components/com_jce/editor/extensions/aggregator/googlemaps/js/googlemaps.js
+++
@@ -1,7 +0,0 @@
-/* JCE Editor - 2.3.4.4 | 12 December 2013 | http://www.joomlacontenteditor.net | Copyright (C) 2006 - 2013 Ryan Demmer. All rights reserved | GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html */
-WFAggregator.add('googlemaps',{params:{width:425,height:350},props:{},setup:function(){},getTitle:function(){return this.title||this.name;},getType:function(){return'iframe';},isSupported:function(v){if(typeof v=='object'){v=v.src||v.data||'';}
-if(/maps\.google\./i.test(v)){return'googlemaps';}
-return false;},getValues:function(src){var self=this,data={},args={},type=this.getType();if(!/\&(amp;)?output=embed/.test(src)){src+='&output=embed';}
-src=src.replace(/^http(s)?:\/\//,'//');data.src=src;if(type=='iframe'){$.extend(data,{frameborder:0,marginwidth:0,marginheight:0});}
-return data;},setValues:function(data){var self=this,id='',src=data.src||data.data||'';if(!src){return data;}
-src=src.replace(/^http(s)?:\/\//,'//');src=src.replace(/\&(amp;)?output=embed/,'');data.src=src;return data;},getAttributes:function(src){var args={},data=this.setValues({src:src})||{};$.extend(args,{'src':data.src||src,'width':this.params.width,'height':this.params.height});return args;},setAttributes:function(){},onSelectFile:function(){},onInsert:function(){}});
--- components/com_jce/editor/tiny_mce/plugins/kitchensink/editor_plugin.js
+++
@@ -1,8 +0,0 @@
-/* JCE Editor - 2.3.4.4 | 12 December 2013 | http://www.joomlacontenteditor.net | Copyright (C) 2006 - 2013 Ryan Demmer. All rights reserved | GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html */
-(function(){var each=tinymce.each,cookie=tinymce.util.Cookie,DOM=tinymce.DOM;tinymce.create('tinymce.plugins.KitchenSink',{init:function(ed,url){var self=this,state=false,h=0,el=ed.getElement(),s=ed.settings;function toggle(){var row=DOM.getParents(ed.id+'_kitchensink','table.mceToolbar');if(!row){return;}
-var n=DOM.getNext(row[0],'table.mceToolbar');while(n){if(DOM.isHidden(n)){DOM.setStyle(n,'display','');state=true;}else{DOM.hide(n);state=false;}
-n=DOM.getNext(n,'table.mceToolbar');}
-h=s.height||el.style.height||el.offsetHeight;if(h){DOM.setStyle(ed.id+'_ifr','height',h);}
-ed.controlManager.setActive('kitchensink',state);}
-ed.addCommand('mceKitchenSink',toggle);ed.addButton('kitchensink',{title:'kitchensink.desc',cmd:'mceKitchenSink'});ed.onPostRender.add(function(ed,cm){if(DOM.get('mce_fullscreen')){state=true;return;}
-toggle();});ed.onInit.add(function(ed){ed.controlManager.setActive('kitchensink',state);});},getInfo:function(){return{longname:'Kitchen Sink',author:'Ryan Demmer',authorurl:'http://www.joomlacontenteditor.net/',infourl:'http://www.joomlacontenteditor.net/',version:'2.3.4.4'};}});tinymce.PluginManager.add('kitchensink',tinymce.plugins.KitchenSink);})();
