fa139f20b91b2aa6bda8fe62fcdbe995fcc04edc
Rafael Berlanda <rafaelberlanda@gmail.com>
2014-09-23 15:04:16 -0300

    Atualização para CMS Joomla 3.3.4

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -25,7 +25,7 @@ final class JVersion
 	public $RELEASE = '3.3';
 
 	/** @var  string  Maintenance version. */
-	public $DEV_LEVEL = '3';
+	public $DEV_LEVEL = '4';
 
 	/** @var  string  Development STATUS. */
 	public $DEV_STATUS = 'Stable';
@@ -37,10 +37,10 @@ final class JVersion
 	public $CODENAME = 'Ember';
 
 	/** @var  string  Release date. */
-	public $RELDATE = '25-July-2014';
+	public $RELDATE = '23-September-2014';
 
 	/** @var  string  Release time. */
-	public $RELTIME = '13:00';
+	public $RELTIME = '14:00';
 
 	/** @var  string  Release timezone. */
 	public $RELTZ = 'GMT';
