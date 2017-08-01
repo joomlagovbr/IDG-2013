edd13887ceb90dc8be0e9f730680c1957431b6d9
Tiago Garcia <tiagovtg@gmail.com>
2017-08-01 09:39:42 -0300

    Update index.php
    Correção do bug dos botões no front end.

--- templates/padraogoverno01/index.php
+++ templates/padraogoverno01/index.php
@@ -8,6 +8,9 @@
 
 // No direct access.
 defined('_JEXEC') or die;
+// Add JavaScript Frameworks
+JHtml::('bootstrap.framework');
+
 require JPATH_SITE .'/templates/'.$this->template.'/helper.php';
 TmplPadraoGoverno01Helper::init( $this ); //inicializacao de funcoes do template, como configuracao de cor, se alterada via get, limpeza do head padrao do joomla e outras providencias.
 $active_item = TmplPadraoGoverno01Helper::getActiveItemid();
