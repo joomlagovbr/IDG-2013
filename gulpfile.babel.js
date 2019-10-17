af1c3ba2a70a260147d1c833707305ac114eff47
Rene B. Pinto <renebentes@yahoo.com.br>
2019-10-17 13:02:51 -0300

    Adiciona tarefa para minificar javascript do tema

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,11 @@
+'use strict';
 import browserSync from 'browser-sync';
 import del from 'del';
 import gulp from 'gulp';
 import cleanCSS from 'gulp-clean-css';
 import less from 'gulp-less';
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify';
 import yargs from 'yargs';
 
 const argv = yargs.option('app', {
@@ -15,6 +18,10 @@ const paths = {
 	styles: {
 		src: ['./templates/padraogoverno01/less/*.less', '!./templates/padraogoverno01/less/_*.less'],
 		dest: './templates/padraogoverno01/css/'
+	},
+	scripts: {
+		src: ['./templates/padraogoverno01/js/*.js', '!./templates/padraogoverno01/js/*.min.js'],
+		dest: './templates/padraogoverno01/js/'
 	}
 };
 
@@ -49,14 +56,26 @@ export const styles = () => {
 };
 
 /**
+ * Minifica arquivos javascripts
+ */
+export const scripts = () => {
+	return gulp
+		.src(paths.scripts.src)
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.scripts.dest));
+};
+
+/**
  * Monitora e processa os arquivos LESS quando alterados sem comprimir.
  * Usado para desenvolvimento
  */
 const watchFiles = () => {
 	gulp.watch(paths.php, reload);
 	gulp.watch(paths.styles.src, gulp.series(styles, reload));
+	gulp.watch(paths.scripts.src, gulp.series(scripts, reload));
 };
 
-export const dev = gulp.series(clean, styles, serve, watchFiles);
-export const build = gulp.series(clean, styles);
+export const dev = gulp.series(clean, gulp.parallel(styles, scripts), serve, watchFiles);
+export const build = gulp.series(clean, gulp.parallel(styles, scripts));
 export default dev;
