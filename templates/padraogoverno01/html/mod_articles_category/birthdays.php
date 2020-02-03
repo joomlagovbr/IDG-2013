ed02c9caec844a2f18387f37cd3bf2bcfee21743
Rene B. Pinto <renebentes@yahoo.com.br>
2020-02-03 10:15:23 -0300

    Ajusta exibição da lista de aniversariantes

--- templates/padraogoverno01/html/mod_articles_category/birthdays.php
+++ templates/padraogoverno01/html/mod_articles_category/birthdays.php
@@ -13,22 +13,33 @@ defined('_JEXEC') or die();
 
 JLoader::register('BirthdaysHelper', __DIR__ . '/_helper.php');
 
+$firstDay = new JDate('today');
+$lastDay = clone $firstDay;
+$lastDay->add(new DateInterval('P7D'));
 $list = BirthdaysHelper::filterByBirthdate($list);
 ?>
+<p class="period">
+	<?php echo JHtml::_('date', $firstDay, JText::_('DATE_FORMAT_LC3')); ?> -
+	<?php echo JHtml::_('date', $lastDay, JText::_('DATE_FORMAT_LC3')); ?>
+</p>
 <ul class="birthdays<?php echo $moduleclass_sfx; ?>">
 
 <?php foreach ($list as &$item): ?>
 	<li>
 	<?php if (isset($item->fields['birthdate'])): ?>
-		<time datetime="<?php echo $item->fields['birthdate']->value; ?>">
-			<span>
-				<?php echo JHtml::_('date', $item->fields['birthdate']->value, 'd', null); ?>
+		<div class="date-block">
+			<h5 class="day">
+				<?php echo JHtml::_('date', $item->fields['birthdate']->value, 'd'); ?>
+			</h5>
+			<span class="month">
+				<?php echo JHtml::_('date', $item->fields['birthdate']->value, 'M'); ?>
 			</span>
-		</time>
+		</div>
 	<?php endif; ?>
-		<span>
+
+		<p class="person">
 			<?php echo BirthdaysHelper::prepareName($item); ?>
-		</span>
+		</p>
 	</li>
 <?php endforeach; ?>
 </ul>
