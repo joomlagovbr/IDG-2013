34ac6e1baffc64e25c52083fc8668f51b82e7693
Rene B. Pinto <renebentes@yahoo.com.br>
2020-03-09 09:31:43 -0300

    Corrige exibição de imagens em galeria

--- templates/padraogoverno01/html/layouts/field/gallery.php
+++ templates/padraogoverno01/html/layouts/field/gallery.php
@@ -25,22 +25,21 @@ $valuesCounter = 0;
 if (empty($values)) {
 	return;
 }
-
 ?>
 
-<div id="gallery-carousel-<?php echo $item->id; ?>" class="carousel gallery slide">
+<div id="gallery-carousel-<?php echo $field->id; ?>" class="carousel gallery slide">
 	<div class="carousel-inner">
 		<?php foreach ($values as $value) : ?>
 			<div class="item<?php echo $valuesCounter == 0 ? ' active' : ''; ?>">
 				<?php $altText = $value['Texto Alternativo'] == '' ? 'Foto da galeria de imagens: sem descrição informada.' : $value['Texto Alternativo']; ?>
-				<img src="<?php echo $value['Foto/Imagem']; ?>" alt="<?php echo $altText; ?>" />
+				<img src="<?php echo $value['Foto']; ?>" alt="<?php echo $altText; ?>" />
 				<div class="carousel-caption">
 					<?php if ($value['Legenda']) : ?>
 						<p><?php echo $value['Legenda']; ?></p>
 					<?php endif; ?>
 
-					<?php if ($value['Créditos/Autor']) : ?>
-						<p class="rights">Foto: <?php echo $value['Créditos/Autor']; ?></p>
+					<?php if ($value['Autor']) : ?>
+						<p class="rights">Foto: <?php echo $value['Autor']; ?></p>
 					<?php endif; ?>
 
 					<span class="options">
@@ -54,7 +53,7 @@ if (empty($values)) {
 		<?php endforeach; ?>
 	</div>
 	<?php if ($valuesCounter > 1) : ?>
-		<a class="carousel-control left" href="#gallery-carousel-<?php echo $item->id; ?>" data-slide="prev">&lsaquo;</a>
-		<a class="carousel-control right" href="#gallery-carousel-<?php echo $item->id; ?>" data-slide="next">&rsaquo;</a>
+		<a class="carousel-control left" href="#gallery-carousel-<?php echo $field->id; ?>" data-slide="prev">&lsaquo;</a>
+		<a class="carousel-control right" href="#gallery-carousel-<?php echo $field->id; ?>" data-slide="next">&rsaquo;</a>
 	<?php endif; ?>
 </div>
