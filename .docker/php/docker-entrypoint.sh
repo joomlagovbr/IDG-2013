10f78a604cc564f6b710a5647ba65ef40cd47ad4
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-06-23 15:03:55 -0300

    Organiza configurações para os containers

---
+++ .docker/php/docker-entrypoint.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+set -eo pipefail
+
+# https://github.com/nginxinc/docker-nginx/blob/master/stable/debian/docker-entrypoint.sh
+# https://github.com/MariaDB/mariadb-docker/blob/master/docker-entrypoint.sh
+
+
+log() {
+	local type="$1"; shift
+	printf '%s [%s] [Entrypoint]: %s\n' "$(date --rfc-3339=seconds)" "$type" "$*"
+}
+
+note() {
+	log Note "$@"
+}
+
+warn() {
+	log Warn "$@" >&2
+}
+
+error() {
+	log ERROR "$@" >&2
+	exit 1
+}
+
+docker_process_init_files() {
+	if find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
+		note "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
+
+		find "/docker-entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
+			case "$f" in
+				*.sh)
+					if [ -x "$f" ]; then
+						note "$0: Launching $f";
+						"$f"
+					else
+						note "$0: Sourcing $f";
+						. "$f"
+					fi
+					;;
+				*configuration.php)
+					process_envs $f;
+					move_file "$f.tmp" configuration.php;
+					populate_sample;
+					remove_folder installation;
+					;;
+				*) warn "$0: Ignoring $f";;
+			esac
+		done
+
+		note "$0: Configuration complete; ready for start up"
+	else
+		note "$0: No files found in /docker-entrypoint.d/, skipping configuration"
+	fi
+}
+
+set_default_envs() {
+	local var="$1"
+	local val="${2:-}"
+
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	fi
+
+	export "$var"="$val"
+}
+
+process_envs() {
+	local defined_envs
+
+	if [ -z "${JOOMLA_DB_HOST}" -o -z "${JOOMLA_DB_USER}" -o -z "${JOOMLA_DB_PASSWORD}" ]; then
+		error $'Unable to init default configuration.\n\tYou need to specify JOOMLA_DB_HOST, JOOMLA_DB_USER, JOOMLA_DB_PASSWORD environment variables.'
+	fi
+
+	defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
+
+	note "$0: Running envsubst on $1"
+	envsubst "$defined_envs" < "$1" > "$1.tmp"
+}
+
+move_file() {
+	[ -f $1 ] || return 0
+	mv $1 $2
+}
+
+remove_folder() {
+	[ -d $1 ] || return 0
+	rm -rf $1
+}
+
+set_owner() {
+	uid="$(id -u)"
+	gid="$(id -g)"
+
+	if [ "$uid" = '0' ]; then
+		user='www-data'
+		group='www-data'
+	else
+		user="$uid"
+		group="$gid"
+	fi
+
+	chown -R "$user:$group" .
+}
+
+populate_sample() {
+	local sample="$(pwd)/installation/sql/mysql/sample_padrao_egov.sql"
+	note "$0: Populate database sample for $(basename $sample)"
+
+	[ -f $sample ] || return 0
+
+	php /populate-sample.php $sample
+}
+
+set_default_envs 'JOOMLA_DB_NAME' 'joomlagovdb'
+set_default_envs 'JOOMLA_DB_PREFIX' 'xmx0n_'
+
+docker_process_init_files
+set_owner
+
+exec "$@"
---
+++ .docker/php/populate-sample.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * Script used to build a sample site on Docker container.
+ *
+ * If configuration.php file is provide on start your container,
+ * this script make a site with default Gov BR sample.
+ *
+ * @package     Joomla.Docker
+ * @subpackage  PHP.Sample
+ *
+ * @copyright   Copyright (C) 2013 - 2021 JoomlaGovBR. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * @link        https://github.com/joomlagovbr
+ * @since       1.0.0
+ */
+
+function usage($command)
+{
+	echo PHP_EOL;
+	echo 'Usage: php ' . $command . ' /path/to/file.sql' . PHP_EOL;
+	echo PHP_EOL;
+}
+
+function message($message)
+{
+	echo PHP_EOL;
+	echo $message . PHP_EOL;
+	echo PHP_EOL;
+}
+
+function replacePrefixTable($content, $prefixTable)
+{
+	$regex = "/#__/";
+	return preg_replace($regex, $prefixTable, $content);
+}
+
+// From InstallationModelDatabase
+function splitQueries($query)
+{
+	$buffer    = array();
+	$queries   = array();
+	$in_string = false;
+
+	$query = trim($query);
+	$query = preg_replace("/\n\#[^\n]*/", '', "\n" . $query);
+	$query = preg_replace("/\n\--[^\n]*/", '', "\n" . $query);
+
+	$funct = explode('CREATE OR REPLACE FUNCTION', $query);
+
+	$query = $funct[0];
+
+	for ($i = 0; $i < strlen($query) - 1; $i++)
+	{
+		if (!$in_string && $query[$i] === ';')
+		{
+			$queries[] = substr($query, 0, $i);
+			$query     = substr($query, $i + 1);
+			$i         = 0;
+		}
+
+		if ($in_string && $query[$i] == $in_string && $buffer[1] !== '\\')
+		{
+			$in_string = false;
+		}
+		elseif (!$in_string && ($query[$i] === '"' || $query[$i] === "'") && (!isset($buffer[0]) || $buffer[0] !== '\\'))
+		{
+			$in_string = $query[$i];
+		}
+
+		if (isset ($buffer[1]))
+		{
+			$buffer[0] = $buffer[1];
+		}
+
+		$buffer[1] = $query[$i];
+	}
+
+	if (!empty($query))
+	{
+		$queries[] = $query;
+	}
+
+	for ($f = 1, $fMax = count($funct); $f < $fMax; $f++)
+	{
+		$queries[] = 'CREATE OR REPLACE FUNCTION ' . $funct[$f];
+	}
+
+	return $queries;
+}
+
+$sampleSqlFile = $argv['1'];
+
+if (!file_exists($sampleSqlFile) || empty($sampleSqlFile))
+{
+	usage($argv[0]);
+	die();
+}
+
+if (strpos(getenv('JOOMLA_DB_HOST', true), ':') === false)
+{
+	$host = getenv('JOOMLA_DB_HOST', true);
+	$port = 3306;
+}
+else
+{
+	list($host, $port) = explode(':', getenv('JOOMLA_DB_HOST', true), 2);
+}
+
+$database    = getenv('JOOMLA_DB_NAME', true);
+$user        = getenv('JOOMLA_DB_USER', true);
+$password    = getenv('JOOMLA_DB_PASSWORD', true);
+$prefixTable = getenv('JOOMLA_DB_PREFIX', true);
+
+$sampleContent = file_get_contents($sampleSqlFile);
+$sampleContent = replacePrefixTable($sampleContent, $prefixTable);
+
+$maxTries = 10;
+
+do
+{
+	$mysql = new mysqli($host, $user, $password, '', $port);
+
+	if ($mysql->connect_error)
+	{
+		message("MySQL Connection Error: ({$mysql->connect_errno}) {$mysql->connect_error}");
+
+		--$maxTries;
+
+		if ($maxTries <= 0)
+		{
+			exit(1);
+		}
+
+		sleep(3);
+	}
+}
+while ($mysql->connect_error);
+
+if (!$mysql->query('CREATE DATABASE IF NOT EXISTS `' . $mysql->real_escape_string($database) . '`'))
+{
+	message("MySQL 'CREATE DATABASE' Error: " . $mysql->error);
+	$mysql->close();
+	exit(1);
+}
+
+if (!$mysql->query('USE `' . $mysql->real_escape_string($database) . '`'))
+{
+	message("MySQL 'USE' Error: " . $mysql->error);
+	$mysql->close();
+	exit(1);
+}
+
+$queries = splitQueries($sampleContent);
+
+foreach ($queries as $query)
+{
+	if (!$mysql->query($query))
+	{
+		message("MySQL 'POPULATE DATABASE' Error: " . $mysql->error);
+		$mysql->close();
+		exit(1);
+	}
+}
+
+message('Populate database sample complete.');
+
+$mysql->close();
