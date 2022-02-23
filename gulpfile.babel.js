3ce49215fa2136123cdd8f57ea8e8434a8f74f07
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-02-23 16:59:16 -0300

    style: Aplica padronização de código

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,5 @@
 'use strict';
+
 import browserSync from 'browser-sync';
 import del from 'del';
 import gulp from 'gulp';
@@ -14,20 +15,23 @@ const argv = yargs.option('app', {
 }).argv;
 
 const paths = {
-	php: './templates/padraogoverno01/**/*.php',
-	styles: {
-		src: './templates/padraogoverno01/less/*.less',
-		dest: './templates/padraogoverno01/css/'
-	},
-	scripts: {
-		src: ['./templates/padraogoverno01/js/*.js', '!./templates/padraogoverno01/js/*.min.js'],
-		dest: './templates/padraogoverno01/js/'
-	}
+  php: './templates/padraogoverno01/**/*.php',
+  styles: {
+    src: './templates/padraogoverno01/less/*.less',
+    dest: './templates/padraogoverno01/css/'
+  },
+  scripts: {
+    src: [
+      './templates/padraogoverno01/js/*.js',
+      '!./templates/padraogoverno01/js/*.min.js'
+    ],
+    dest: './templates/padraogoverno01/js/'
+  }
 };
 
-const reload = done => {
-	browserSync.reload();
-	done();
+const reload = (done) => {
+  browserSync.reload();
+  done();
 };
 
 const serve = done => {
@@ -36,38 +40,39 @@ const serve = done => {
 		open: false
 	});
 
-	done();
+  done();
 };
 
 /**
  * Limpa a pasta de destino dos css
  */
-export const clean = () => del([paths.styles.dest, '!./templates/padraogoverno01/css/custom.css']);
+export const clean = () =>
+  del([paths.styles.dest, '!./templates/padraogoverno01/css/custom.css']);
 
 /**
  * Processa os arquivos LESS e comprime o CSS
  */
 export const styles = () => {
-	return gulp
-		.src([paths.styles.src, '!./templates/padraogoverno01/less/_*.less'])
-		.pipe(less())
-		.pipe(cleanCSS({ compatibility: 'ie7' }))
-		.pipe(gulp.dest(paths.styles.dest));
+  return gulp
+    .src([paths.styles.src, '!./templates/padraogoverno01/less/_*.less'])
+    .pipe(less())
+    .pipe(cleanCSS({ compatibility: 'ie7' }))
+    .pipe(gulp.dest(paths.styles.dest));
 };
 
 /**
  * Minifica arquivos javascripts
  */
 export const scripts = () => {
-	return gulp
-		.src(paths.scripts.src)
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(uglify())
-		.pipe(gulp.dest(paths.scripts.dest));
+  return gulp
+    .src(paths.scripts.src)
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.dest));
 };
 
 /**
- * Monitora e processa os arquivos LESS quando alterados sem comprimir.
+ * Monitora e processa os arquivos quando alterados.
  * Usado para desenvolvimento
  */
 const watchFiles = () => {
@@ -76,6 +81,11 @@ const watchFiles = () => {
 	gulp.watch(paths.scripts.src, gulp.series(scripts, reload));
 };
 
-export const dev = gulp.series(clean, gulp.parallel(styles, scripts), serve, watchFiles);
+export const dev = gulp.series(
+  clean,
+  gulp.parallel(styles, scripts),
+  serve,
+  watchFiles
+);
 export const build = gulp.series(clean, gulp.parallel(styles, scripts));
 export default dev;
