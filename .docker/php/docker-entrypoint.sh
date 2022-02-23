544e794b0b05371f144868e548de3c6a9ee0a190
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-02-23 16:43:22 -0300

    chore(docker): Define processo para criação de portal base
    O processo verifica a existência do arquivo template `configuration.php`
    na pasta /docker-entrypoint.d/ e a definição das variáveis de ambientes
    necessárias para gerar um portal base durante a execução do container.
    
    Lista de variáveis de ambiente:
     - JOOMLA_DB_HOST
     - JOOMLA_DB_USER
     - JOOMLA_DB_PASSWORD
     - JOOMLA_DB_NAME
     - JOOMLA_DB_PREFIX
     - JOOMLA_ROOT_USER
     - JOOMLA_ROOT_PASSWORD

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -38,6 +38,12 @@ docker_process_init_files() {
 						. "$f"
 					fi
 					;;
+				*configuration.php)
+					process_envs $f;
+					move_file "$f.tmp" configuration.php;
+					populate_sample;
+					remove_folder installation;
+					;;
 				*) warn "$0: Ignoring $f";;
 			esac
 		done
@@ -47,6 +53,55 @@ docker_process_init_files() {
 		note "$0: No files found in /docker-entrypoint.d/, skipping configuration"
 	fi
 }
+
+process_envs() {
+	local defined_envs
+
+	if [ -z "${JOOMLA_DB_HOST}" -o -z "${JOOMLA_DB_USER}" -o -z "${JOOMLA_DB_PASSWORD}" ]; then
+		error $'Unable to init default configuration.\n\tYou need to specify JOOMLA_DB_HOST, JOOMLA_DB_USER, JOOMLA_DB_PASSWORD environment variables.'
+	fi
+
+	defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
+
+	note "$0: Running envsubst on $1"
+	envsubst "$defined_envs" < "$1" > "$1.tmp"
+}
+
+move_file() {
+	[ -f $1 ] || return 0
+	mv $1 $2
+}
+
+remove_folder() {
+	[ -d $1 ] || return 0
+	rm -rf $1
+}
+
+populate_sample() {
+	local sample="$(pwd)/installation/sql/mysql/sample_padrao_egov.sql"
+	note "$0: Populate database sample for $(basename $sample)"
+
+	[ -f $sample ] || return 0
+
+	populate-sample.php $sample
+}
+
+get_env_vars() {
+    local env_var="${1}"
+    local env_var_file="${env_var}_FILE"
+
+    if [[ -n "${!env_var_file}" ]]; then
+        export "${env_var}"="$(cat "${!env_var_file}")"
+    fi
+}
+
+get_env_vars JOOMLA_DB_HOST
+get_env_vars JOOMLA_DB_USER
+get_env_vars JOOMLA_DB_PASSWORD
+get_env_vars JOOMLA_DB_NAME
+get_env_vars JOOMLA_DB_PREFIX
+get_env_vars JOOMLA_ROOT_USER
+get_env_vars JOOMLA_ROOT_PASSWORD
 
 docker_process_init_files
 
